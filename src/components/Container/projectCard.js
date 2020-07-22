2511d744f767a3a396da0711ecef80bfbc155d9d
Wesley Chen <weschen.github@gmail.com>
2020-07-22 13:07:32 -0400

    working on integrating flip-cards

--- src/components/Container/projectCard.js
+++ src/components/Container/projectCard.js
@@ -1,59 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 
 function ProjectCard({ item, i }) {
+	const [ display, setDisplay ] = useState('front');
 	return (
 		<div key={i} className="moving-item">
-			<div className="description">
-				<h2>{item.name}</h2>
-				<p className="project-snippet">{item.description}</p>
-				{item.demourl ? (
-					<a
-						href={item.demourl}
-						alt="demo-link"
-						className="demositelink "
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Working Demo Site
-					</a>
-				) : null}
-
-				<div className="github">
-					<div className="details">
-						<br />
-
-						<i className="fa fa-github"> Code : </i>
-						{item.github.map((x, i) => {
-							return (
+			<div className="flip-card">
+				<div className="inner">
+					<div className="front">
+						<div className="description">
+							<h2>{item.name}</h2>
+							<p className="project-snippet">{item.description}</p>
+							<div className="demo-video">
+								{item.youtubedemo ? (
+									<ReactPlayer
+										controls
+										width="100%"
+										height="220px"
+										id="youtube-video"
+										url={item.youtubedemo}
+										rel="noopener noreferrer"
+									/>
+								) : (
+									<div className="comingsoon">Demo Video Coming Soon</div>
+								)}
+							</div>
+							<br />
+							<button>Details</button>
+						</div>
+					</div>
+					<div className="back">
+						<div className="details">
+							{item.demourl ? (
 								<a
-									key={i}
-									href={x.url}
-									alt={x.type}
-									className="githublink"
+									href={item.demourl}
+									alt="demo-link"
+									className="demositelink "
 									target="_blank"
 									rel="noopener noreferrer"
 								>
-									{x.type}
+									Working Demo Site
 								</a>
-							);
-						})}
+							) : null}
+							<br />
+
+							<i className="fa fa-github"> Code : </i>
+							{item.github.map((x, i) => {
+								return (
+									<a
+										key={i}
+										href={x.url}
+										alt={x.type}
+										className="githublink"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										{x.type}
+									</a>
+								);
+							})}
+						</div>
 					</div>
 				</div>
-			</div>
-			<div className="demo-video">
-				{item.youtubedemo ? (
-					<ReactPlayer
-						controls
-						width="100%"
-						height="220px"
-						id="youtube-video"
-						url={item.youtubedemo}
-						rel="noopener noreferrer"
-					/>
-				) : (
-					<div className="comingsoon">Demo Video Coming Soon</div>
-				)}
 			</div>
 		</div>
 	);
