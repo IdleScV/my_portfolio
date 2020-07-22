c059c2a30e17f9e59d96edf030301fbb20d38b88
Wesley Chen <weschen.github@gmail.com>
2020-07-22 14:17:04 -0400

    edits

--- src/components/Container/projectCard.js
+++ src/components/Container/projectCard.js
@@ -7,7 +7,8 @@ function ProjectCard({ item, i }) {
 	return (
 		<div key={i} className="moving-item">
 			<h2>{item.name}</h2>
-			<ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+
+			<ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
 				<div className="front">
 					<div className="demo-video">
 						{item.youtubedemo ? (
@@ -15,7 +16,6 @@ function ProjectCard({ item, i }) {
 								controls
 								width="100%"
 								height="220px"
-								id="youtube-video"
 								url={item.youtubedemo}
 								rel="noopener noreferrer"
 							/>
@@ -23,14 +23,6 @@ function ProjectCard({ item, i }) {
 							<div className="comingsoon">Demo Video Coming Soon</div>
 						)}
 					</div>
-
-					<button
-						onClick={() => {
-							setisFlipped(!isFlipped);
-						}}
-					>
-						Details
-					</button>
 				</div>
 				<div className="back">
 					<p className="project-snippet">{item.description}</p>
@@ -43,11 +35,10 @@ function ProjectCard({ item, i }) {
 								target="_blank"
 								rel="noopener noreferrer"
 							>
-								Working Demo Site
+								WEBSITE
 							</a>
 						) : null}
 						<br />
-
 						<i className="fa fa-github"> Code : </i>
 						{item.github.map((x, i) => {
 							return (
@@ -64,15 +55,15 @@ function ProjectCard({ item, i }) {
 							);
 						})}
 					</div>
-					<button
-						onClick={() => {
-							setisFlipped(!isFlipped);
-						}}
-					>
-						Demo Video
-					</button>
 				</div>
 			</ReactCardFlip>
+			<button
+				onClick={() => {
+					setisFlipped(!isFlipped);
+				}}
+			>
+				{isFlipped ? 'Back to Demo Video' : 'More Details'}
+			</button>
 		</div>
 	);
 }
